47c535bd176f7564ad308c115884069e996ede97
Saikiran M <Saikiran.M@outlook.in>
2023-01-24 02:31:15 +0530

    added regional endpoint

--- provider.tf
+++ provider.tf
@@ -4,6 +4,8 @@ provider "aws" {
 #    role_arn = "arn:aws:iam::277471061234:user/terraform"
   access_key = "AWS_ACCESS_KEY"
   secret_key = "AWS_SECRET_KEY"
+  
+    sts_regional_endpoints = "regional"
 #  }
 #  shared_credentials_file = "~/.aws/credentials"
 }
