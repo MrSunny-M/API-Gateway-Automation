ad40268e1426ebe2af61bb784e52cd9a62bbe4be
Saikiran M <Saikiran.M@outlook.in>
2023-01-24 02:50:27 +0530

    secrets through file

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 #  assume_role {
 #    role_arn = "arn:aws:iam::277471061234:user/terraform"
-  access_key = "AWS_ACCESS_KEY"
-  secret_key = "AWS_SECRET_KEY"
+#  access_key = "AWS_ACCESS_KEY"
+#  secret_key = "AWS_SECRET_KEY"
   
-  sts_regional_endpoints = "regional"
-  endpoint = "sts.us-east-1.amazonaws.com"
+#  sts_regional_endpoints = "regional"
+#  endpoint = "sts.us-east-1.amazonaws.com"
 #  }
-#  shared_credentials_file = "~/.aws/credentials"
+  shared_credentials_file = "/tmp/credentials"
 }
